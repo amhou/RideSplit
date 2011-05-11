16246615ce0b644ad74a4cccd7291d6d590ab1e1
amhou <hou.andrew@gmail.com>
2011-05-10 18:11:30 -0700

    init try 2

--- gen/net/thinkandrew/ridesplit/R.java
+++ gen/net/thinkandrew/ridesplit/R.java
@@ -13,10 +13,27 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int ButtonCalculate=0x7f050006;
+        public static final int CheckBoxCarWash=0x7f050005;
+        public static final int ScrollView01=0x7f050000;
+        public static final int cost_driver=0x7f050007;
+        public static final int cost_gas=0x7f050002;
+        public static final int cost_gas_label=0x7f050001;
+        public static final int cost_rider=0x7f050008;
+        public static final int riders=0x7f050004;
+        public static final int riders_label=0x7f050003;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int calculate=0x7f040004;
+        public static final int carwash=0x7f040003;
+        public static final int drivers_cost=0x7f040005;
+        public static final int gas_cost=0x7f040001;
+        public static final int num_riders=0x7f040002;
+        public static final int riders_cost=0x7f040006;
     }
 }
--- src/net/thinkandrew/ridesplit/RideSplit.java
+++ src/net/thinkandrew/ridesplit/RideSplit.java
@@ -2,14 +2,45 @@ package net.thinkandrew.ridesplit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RideSplit extends Activity
-{
-    /** Called when the activity is first created. */
+public class RideSplit extends Activity {
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void calcCost(View button) {
+        final EditText gasField = (EditText) findViewById(R.id.cost_gas);
+        float gasCost = Float.valueOf(gasField.getText().toString());
+
+        final EditText ridersField = (EditText) findViewById(R.id.riders);
+        float numRiders = Float.valueOf(ridersField.getText().toString());
+
+        final CheckBox responseCheckBox = (CheckBox) findViewById(R.id.CheckBoxCarWash);
+        boolean carwash = responseCheckBox.isChecked();
+
+        if (carwash) {
+            gasCost += 5;
+        }
+
+        Float driverCost = gasCost / ((float) 1.1 * numRiders);
+        Float riderCost = (gasCost - driverCost) / (numRiders - 1);
+
+        if (numRiders == 1) {
+            driverCost = gasCost;
+            riderCost = (float) 0;
+        }
+
+        TextView driverCostField = (TextView) findViewById(R.id.cost_driver);
+        TextView riderCostField = (TextView) findViewById(R.id.cost_rider);
+
+        driverCostField.setText(getString(R.string.drivers_cost) + " $" + String.format("%.2f", driverCost));
+        riderCostField.setText(getString(R.string.riders_cost) + " $" + String.format("%.2f", riderCost));
     }
 }
