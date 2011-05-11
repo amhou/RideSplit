3d19a98305fc44df0acae2fbf15c22dbaa94617d
amhou <hou.andrew@gmail.com>
2011-05-10 18:26:29 -0700

    added comments

--- src/net/thinkandrew/ridesplit/RideSplit.java
+++ src/net/thinkandrew/ridesplit/RideSplit.java
@@ -16,29 +16,36 @@ public class RideSplit extends Activity {
     }
 
     public void calcCost(View button) {
-        final EditText gasField = (EditText) findViewById(R.id.cost_gas);
+        final EditText gasField = (EditText) findViewById(R.id.cost_gas); //cost of gas
         float gasCost = Float.valueOf(gasField.getText().toString());
 
-        final EditText ridersField = (EditText) findViewById(R.id.riders);
+        final EditText ridersField = (EditText) findViewById(R.id.riders); //number of riders
         float numRiders = Float.valueOf(ridersField.getText().toString());
 
-        final CheckBox responseCheckBox = (CheckBox) findViewById(R.id.CheckBoxCarWash);
+        final CheckBox responseCheckBox = (CheckBox) findViewById(R.id.CheckBoxCarWash); // would you like a carwash?
         boolean carwash = responseCheckBox.isChecked();
 
-        if (carwash) {
+        if (numRiders < (float) 0) // prevents division by 0
+        {
+            Toast.makeText(RideSplit.this, "Number of Riders must be greater than 0.", Toast.LENGTH_SHORT).show();
+        }
+
+        if (carwash) // carwash is worth $5
+        {
             gasCost += 5;
         }
 
-        Float driverCost = gasCost / ((float) 1.1 * numRiders);
-        Float riderCost = (gasCost - driverCost) / (numRiders - 1);
+        Float driverCost = gasCost / ((float) 1.2 * numRiders);      // Driver pays less than riders to compensate for wear and tear on car
+        Float riderCost = (gasCost - driverCost) / (numRiders - 1);  // Rider cost
 
-        if (numRiders == 1) {
+        if (numRiders == 1) // Special case
+        {
             driverCost = gasCost;
             riderCost = (float) 0;
         }
 
-        TextView driverCostField = (TextView) findViewById(R.id.cost_driver);
-        TextView riderCostField = (TextView) findViewById(R.id.cost_rider);
+        TextView driverCostField = (TextView) findViewById(R.id.cost_driver); // Output of driver's cost
+        TextView riderCostField = (TextView) findViewById(R.id.cost_rider);   // Output of rider's cost
 
         driverCostField.setText(getString(R.string.drivers_cost) + " $" + String.format("%.2f", driverCost));
         riderCostField.setText(getString(R.string.riders_cost) + " $" + String.format("%.2f", riderCost));
